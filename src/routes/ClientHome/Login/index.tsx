935b0405a5b467163fe9ad8ac59240b71aa58e11
MiltonRafael <miltonrafael166@gmail.com>
2026-06-06 16:39:17 -0300

    Global state of the token payload con Context API

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import type { CredentialsDTO } from '../../../models/auth';
 import * as authService from '../../../services/auth-service';
 import './styles.css';
 import { useNavigate } from 'react-router-dom';
+import { ContextToken } from '../../../utils/context-token';
 
 export default function Login() {
+
+    const { setContextTokenPayload } = useContext(ContextToken);
 
     const navigate = useNavigate();
 
@@ -17,6 +20,7 @@ export default function Login() {
         event.preventDefault();
         authService.loginRequest(formData).then(response => {
             authService.saveAccessToken(response.data.access_token);
+            setContextTokenPayload(authService.getAccessTokenPayload());
             navigate("/cart");
         })
             .catch(error => {
