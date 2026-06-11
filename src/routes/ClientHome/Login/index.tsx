a7544435b2817b359f5d3ce7527e3e41be03ee7a
MiltonRafael <miltonrafael166@gmail.com>
2026-06-11 15:20:29 -0300

    Custom FormInput

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -3,6 +3,7 @@ import * as authService from '../../../services/auth-service';
 import './styles.css';
 import { useNavigate } from 'react-router-dom';
 import { ContextToken } from '../../../utils/context-token';
+import FormInput from '../../../components/FormInput';
 
 export default function Login() {
 
@@ -32,23 +33,23 @@ export default function Login() {
     });
 
     function handleSubmit(event: any) {
-            event.preventDefault();
-            authService.loginRequest({username: formData.username.value, password: formData.password.value})
-                .then(response => {
-                    authService.saveAccessToken(response.data.access_token);
-                    setContextTokenPayload(authService.getAccessTokenPayload());
-                    navigate("/cart");
-                })
-                .catch(error => {
-                    console.log("erro no login", error);
-                })
-        }
+        event.preventDefault();
+        authService.loginRequest({ username: formData.username.value, password: formData.password.value })
+            .then(response => {
+                authService.saveAccessToken(response.data.access_token);
+                setContextTokenPayload(authService.getAccessTokenPayload());
+                navigate("/cart");
+            })
+            .catch(error => {
+                console.log("erro no login", error);
+            })
+    }
 
     function handleInputChange(event: any) {
-            const value = event.target.value;
-            const name = event.target.name;
-            setFormData({ ...formData, [name]: { ...formData[name], value: value } });
-        }
+        const value = event.target.value;
+        const name = event.target.name;
+        setFormData({ ...formData, [name]: { ...formData[name], value: value } });
+    }
 
     return (
         <main>
@@ -58,24 +59,18 @@ export default function Login() {
                         <h2>Login</h2>
                         <div className="dsc-form-controls-container">
                             <div>
-                                <input
-                                    name="username"
-                                    value={formData.username.value}
+                                <FormInput
+                                    {...formData.username }
                                     onChange={handleInputChange}
                                     className="dsc-form-control"
-                                    type="text"
-                                    placeholder="Email"
                                 />
                                 <div className="dsc-form-error"></div>
                             </div>
                             <div>
-                                <input
-                                    name="password"
-                                    value={formData.password.value}
+                                <FormInput
+                                    {...formData.password }
                                     onChange={handleInputChange}
                                     className="dsc-form-control"
-                                    type="password"
-                                    placeholder="Senha"
                                 />
                             </div>
                         </div>
